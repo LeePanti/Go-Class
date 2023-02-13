104676c5adaa594e1c6026a11940d90f84c3cf45
Lee Panti <2019119651@ub.edu.bz>
2023-02-13 13:47:17 -0600

    flags 2

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
 
 func main() {
 	//set flags
 	msg := flag.String("msg", "Howdy stranger!", "the message to display")
+	num := flag.Int("num", 1, "how many times to print the message")
+	caps := flag.Bool("caps", false, "should the string be all caps")
 	flag.Parse()
 
-	// check if the user set the flag
-	fmt.Println(*msg)
+	for i := 0; i < *num; i++ {
+		if *caps {
+			fmt.Println(strings.ToUpper(*msg))
+		} else {
+			fmt.Println(*msg)
+		}
+	}
 }
