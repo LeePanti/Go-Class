8eb0242eec2b5faa8eba3944f1ed0dde81bb9196
Lee Panti <2019119651@ub.edu.bz>
2023-02-15 13:30:10 -0600

    reverse the string flag

--- main.go
+++ main.go
@@ -14,10 +14,19 @@ func main() {
 	msg := flag.String("msg", "Howdy stranger!", "the message to display")
 	num := flag.Int("num", 1, "how many times to print the message")
 	caps := flag.Bool("caps", false, "should the string be all caps")
+	rev := flag.Bool("r", false, "reverses the string")
 	flag.Parse()
 
 	if *caps {
 		*msg = strings.ToUpper(*msg)
+	}
+
+	if *rev {
+		var message string
+		for _, i := range *msg {
+			message = string(i) + message
+		}
+		*msg = message
 	}
 
 	for i := 0; i < *num; i++ {
